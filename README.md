11f3e7ea5850a88868db7b10e36cc12c18bfce1f
John <john.davies@diamond.ac.uk>
2024-06-07 12:28:30 +0100

    adding repo

---
+++ README.md
@@ -0,0 +1,8 @@
+<img src="https://raw.githubusercontent.com/DiamondLightSource/python-copier-template/main/docs/images/dls-logo.svg"
+     style="background: none" width="120px" height="120px" align="right">
+
+[![CI](https://github.com/DiamondLightSource/ptycho-reduction/actions/workflows/ci.yml/badge.svg)](https://github.com/DiamondLightSource/ptycho-reduction/actions/workflows/ci.yml)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+# Diamond Image Service
+Processes images enabling visulisation, resizing, reformating and retriving/storing of images in NAS S3 Buckets
---
+++ thumbor/src/thumbor_/s3_store/s3_storage.py
@@ -0,0 +1,33 @@
+import boto3
+from thumbor.storages import BaseStorage
+import loaders.bucket_details as bucket_details
+from botocore.client import Config
+
+class Storage(BaseStorage):
+    def __init__(self, context):
+        super(Storage, self).__init__(context)
+        self.s3_client = boto3.client(
+            's3',
+            endpoint_url=bucket_details.ep_url,
+            aws_access_key_id=bucket_details.key_id,
+            aws_secret_access_key=bucket_details.access_key,
+            config=Config(signature_version="s3v4")
+        )
+        self.bucket_name = bucket_details.bucket_name
+
+    def put(self, path, file_bytes):
+        self.s3_client.put_object(Bucket=self.bucket_name, Key=path, Body=file_bytes)
+        return path
+
+    def delete(self, path):
+        self.s3_client.delete_object(Bucket=self.bucket_name, Key=path)
+        return path
+
+    def exists(self, path):
+        try:
+            response = self.s3_client.get_object(Bucket=self.bucket_name, Key=path)
+            return response['Body'].read()
+        except self.s3_client.exceptions.NoSuchKey:
+            return None
+
+
