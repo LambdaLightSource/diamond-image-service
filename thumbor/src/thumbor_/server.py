2c858a8aeb57acc86ba1f4ee6838ea01fe60e7be
John <john.davies@diamond.ac.uk>
2024-06-10 16:55:05 +0100

    Seperation of concerns

--- thumbor/src/thumbor_/server.py
+++ thumbor/src/thumbor_/server.py
@@ -27,46 +27,11 @@ from thumbor.context import Context
 from thumbor.importer import Importer
 from thumbor.signal_handler import setup_signal_handler
 import tornado.web
-import boto3
-from botocore.exceptions import NoCredentialsError, ClientError
-import tornado.web
-import boto3
-from io import BytesIO
-import loaders.bucket_details as bucket_details
-from botocore.config import Config as BotoConfig
-from s3_store.s3_storage import Storage
 
-class UploadHandler(tornado.web.RequestHandler):
-    def initialize(self, bucket_name):
-        self.bucket_name = bucket_name
-        self.storage = Storage(context=self)
-        self.s3 = boto3.client(
-            's3',
-            region_name="eu-west-2",
-            endpoint_url=bucket_details.ep_url,
-            aws_access_key_id=bucket_details.key_id,
-            aws_secret_access_key=bucket_details.access_key,
-            config=BotoConfig(signature_version="s3v4"),
-        )
-
-    def put(self):
-        fileinfo = self.request.files['media'][0]
-        filename = fileinfo['filename']
-        filebody = fileinfo['body']
-        try:
-            path = self.storage.put(filename, BytesIO(filebody))
-            self.write({'status': 'success', 'message': f'File {path} uploaded successfully to S3'})
-        except Exception as e:
-            self.write({'status': 'error', 'message': str(e)})
+from handlers.upload_handler import UploadHandler
 
 
-    def delete(self):
-        filename = self.get_argument('filename')
-        try:
-            path = self.storage.delete(filename)
-            self.write({'status': 'success', 'message': f'File {path} deleted successfully from S3'})
-        except Exception as e:
-            self.write({'status': 'error', 'message': str(e)})
+
 
 def get_as_integer(value):
     try:
