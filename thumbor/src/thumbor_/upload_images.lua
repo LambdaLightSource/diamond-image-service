db24c2aa54b95e0a7301b5168e4d96d12ef1629b
John <john.davies@diamond.ac.uk>
2024-08-12 15:46:23 +0100

    error checks

--- thumbor/src/thumbor_/upload_images.lua
+++ thumbor/src/thumbor_/upload_images.lua
@@ -1,15 +1,23 @@
 function read_file(file)
-    local f = io.open(file, "rb")
+    local f, err = io.open(file, "rb")
+    if f == nil then
+        print("Error opening file: " .. err)
+    end
     local content = f:read("*all")
     f:close()
     return content
 end
 
 request = function()
-    local image_num = math.random(0, 999)
+    local image_num = math.random(0, 999) 
     local path = "/upload"
-    local body = read_file("/generated_images/random_image_" .. string.format("%06d", image_num) .. ".png")
+    local file_path = "thumbor/src/generated_images/random_image_" .. string.format("%06d", image_num) .. ".png"
+    local body = read_file(file_path)
+    if body == nil then
+        return wrk.format(nil, path) 
+    end
     local headers = {}
-    headers["Content-Type"] = "application/octet-stream"
+    headers["Content-Type"] = "image/png" 
+    
     return wrk.format("POST", path, headers, body)
 end
